b241259f4045cfb3649448e76be72992188a662c
محمد مهدی پورنجفی <m.p_996@hotmail.com>
2023-10-06 19:42:26 +0330

    Add HyperParameterExploration

--- SuggestResturantsApp/Program.cs
+++ SuggestResturantsApp/Program.cs
@@ -6,8 +6,8 @@ using Microsoft.ML.Trainers;
 using RestaurantRecommender;
 
 Console.WriteLine("Add started");
-Trainig();
-
+//Trainig();
+TrainigV2();
 
 
 
@@ -102,4 +102,100 @@ static void Trainig()
         Console.WriteLine($"Predicted Rating [{top10Restaurant.PredictRating:#.0}] for restaurant {top10Restaurant.RestaurantName}");
     }
 
+}
+
+
+static void TrainigV2()
+{
+    MLContext mlContext = new MLContext();
+    var trainingDataFile = "Data\\trainingData.tsv";
+
+    DataPreparer.PreprocessData(trainingDataFile);
+
+    IDataView trainingDataView = mlContext.Data.LoadFromTextFile<ModelInput>(trainingDataFile, hasHeader: true);
+
+    var dataPreProcessingPipeline =
+        mlContext.Transforms
+            .Conversion
+            .MapValueToKey(outputColumnName: "UserIdEncoded", inputColumnName: nameof(ModelInput.UserId))
+            .Append(mlContext.Transforms
+                .Conversion
+                .MapValueToKey(outputColumnName: "RestaurantNameEncoded", inputColumnName: nameof(ModelInput.RestaurantName)));
+
+    var finalOption = new MatrixFactorizationTrainer.Options
+    {
+        MatrixColumnIndexColumnName = "UserIdEncoded",
+        MatrixRowIndexColumnName = "RestaurantNameEncoded",
+        LabelColumnName = "TotalRating",
+        NumberOfIterations = 100,
+        ApproximationRank = 100,
+        Quiet = true
+    };
+    var trainer = mlContext.Recommendation().Trainers.MatrixFactorization(finalOption);
+
+    var trainerPipeline = dataPreProcessingPipeline.Append(trainer);
+
+
+
+    var crossValMetrics = mlContext.Recommendation()
+        .CrossValidate(data: trainingDataView, estimator: trainerPipeline,
+        labelColumnName: "TotalRating");
+
+    var averageRMSE = crossValMetrics.Average(m => m.Metrics.RootMeanSquaredError);
+    var averageRSquared = crossValMetrics.Average(m => m.Metrics.RSquared);
+
+    Console.WriteLine();
+    Console.WriteLine("------------------- Metrics before tuning hyper parameters ----");
+    Console.WriteLine($"Cross validated root error : {averageRMSE:#.000}");
+    Console.WriteLine($"Cross validated RSquared : {averageRSquared:#.000}");
+    Console.WriteLine();
+    HyperParameterExploration(mlContext, dataPreProcessingPipeline, trainingDataView);
+
+
+
+}
+
+
+static void HyperParameterExploration(MLContext mlContext , IEstimator<ITransformer> dataProcessingPipeLine , IDataView trainingDataView)
+{
+    var results = new List<(double rootMeanSquaredError, double rSquared, int interations, int approximationRank)>();
+    for (int interations = 5; interations < 100; interations+=5)
+    {
+        for (int approximationRank = 50; approximationRank < 250; approximationRank+=50)
+        {
+            var option = new MatrixFactorizationTrainer.Options
+            {
+                MatrixColumnIndexColumnName = "UserIdEncoded",
+                MatrixRowIndexColumnName = "RestaurantNameEncoded",
+                LabelColumnName = "TotalRating",
+                NumberOfIterations = interations,
+                ApproximationRank = approximationRank,
+                Quiet = true
+            };
+            var currentTrainer = mlContext.Recommendation().Trainers.MatrixFactorization(option);
+
+            var completePipeline = dataProcessingPipeLine.Append(currentTrainer);
+
+            var crossValMetrics = mlContext.Recommendation()
+                .CrossValidate(trainingDataView, completePipeline, labelColumnName: "TotalRating");
+
+            results.Add(
+                (crossValMetrics.Average(m=>m.Metrics.RootMeanSquaredError),
+                    crossValMetrics.Average(m=>m.Metrics.RSquared),
+                    interations , approximationRank)
+                );
+        }
+    }
+
+
+    Console.WriteLine("---  Hyper Parameter and Metrics ----");
+    foreach (var valueTuple in results.OrderByDescending(r=>r.rSquared))
+    {
+        Console.WriteLine($"NUmber of Iterations: {valueTuple.interations}");
+        Console.WriteLine($"NUmber of ApproximationRank: {valueTuple.approximationRank}");
+        Console.WriteLine($"NUmber of RootMeanSquaredError: {valueTuple.rootMeanSquaredError}");
+        Console.WriteLine($"NUmber of rSquared: {valueTuple.rSquared}");
+    }
+
+    Console.WriteLine("Done");
 }
